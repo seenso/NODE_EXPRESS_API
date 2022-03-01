161f7427d085a718842ed8911209e6ef0e07ce14
Seen So <so.seen@hotmail.com>
2022-03-01 13:08:52 -0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+import express from 'express'; //backend
+import bodyParser from 'body-parser'; //parses incoming body data
+
+const app = express(); // our app lives here
+const PORT = 5000; // all caps as it will never change
+
+app.use(bodyParser.json()); //formats app so we use JSON data in whole app
+
+app.listen(PORT, () => console.log(`Server running on port: http://localhost:${PORT}`))
+
+
+
+
+
+
