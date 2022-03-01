0dd82e61d2e47c8a6fe22291d89818c36118b157
seenso <>
2022-03-01 14:15:25 -0900

    Created POST route.

--- routes/users.js
+++ routes/users.js
@@ -2,9 +2,29 @@ import express from 'express';
 
 const router = express.Router();
 
-// all routes in here start with /users thanks to the "/users" enpoint in index.js, so no need to add it in the endpoint
+const users = [
+  {
+    firstName: "John",
+    lastName: "Doe", 
+    age: 25
+  },
+  {
+    firstName: "Jane",
+    lastName: "Doe", 
+    age: 24
+  }
+]
+
+// all routes in here start with /users thanks to the "/users" enpoint in index.js, so no need to add it in the endpoint on this file.
 router.get('/', (req, res) => {
-  res.send('Hello')
+  res.send(users)
 });
+
+router.post('/', (req, res) => {
+  const user = req.body;
+  users.push(user)
+
+  res.send(`User with the name ${user.firstName} ${user.lastName} added to the database!`)
+})
 
 export default router;
