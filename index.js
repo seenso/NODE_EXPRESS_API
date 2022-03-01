64fa05598dac28a51749776b41ff7f6f6cae8336
seenso <>
2022-03-01 13:22:01 -0900

    Made first GET w express & added node_modules to .gitignore

--- index.js
+++ index.js
@@ -6,6 +6,14 @@ const PORT = 5000; // all caps as it will never change
 
 app.use(bodyParser.json()); //formats app so we use JSON data in whole app
 
+// ROUTES
+app.get('/', (req, res) => {
+  res.send('Hello from Homepage.')
+});
+
+
+
+
 app.listen(PORT, () => console.log(`Server running on port: http://localhost:${PORT}`))
 
 
